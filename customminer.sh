cca8717bd65d00edd2f30691cb1bbce83b272ec5
kernelpanic007 <josalamanca007@gmail.com>
2024-02-05 15:51:23 -0600

    Update customminer.sh

--- customminer.sh
+++ customminer.sh
@@ -1,3 +1,3 @@
 /hive/sbin/amd-oc
-screen -dmS wart_miner bash -c "while true; do /home/user/janusminer_hiveos/wart-miner -a walletaddress -h 192.168.254.21 -p 3000 --threads=8; done"
+screen -dmS wart_miner bash -c  "while true; do /home/user/janusminer_hiveos/wart-miner -a walletaddress -h nodeipaddressvar -p 3000 --threads=8 2>&1 | tee /var/log/janusminer_hiveos.log; done"
 
