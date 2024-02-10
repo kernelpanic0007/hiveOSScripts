2f1c584da5c0773a406667cc84321e77af4a8a80
kernelpanic007 <josalamanca007@gmail.com>
2024-02-09 18:34:27 -0600

    Create customminer16.sh

---
+++ customminer16.sh
@@ -0,0 +1,8 @@
+#exit out of screen
+screen -S wart_miner -X quit
+#download if necessary and run miner
+cd /home/user
+wget -nc https://github.com/CoinFuMasterShifu/janusminer/releases/download/0.1.5/janusminer_hiveos-0.1.5.tgz
+tar xzvf janusminer_hiveos-0.1.5.tgz
+/hive/sbin/amd-oc
+screen -dmS wart_miner bash -c  "while true; do /home/user/janusminer_hiveos/wart-miner -a walletaddress -h nodeipaddressvar -p nodeipport --threads=8 2>&1 | tee /var/log/janusminer_hiveos.log; done"
