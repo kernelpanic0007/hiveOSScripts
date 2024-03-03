ad6c5d6c4ee43cdb8509b3c4b8dbcfa523e488a8
kernelpanic007 <josalamanca007@gmail.com>
2024-03-03 16:55:23 -0600

    Update customminer.sh

--- customminer.sh
+++ customminer.sh
@@ -6,5 +6,5 @@ wget -nc https://github.com/CoinFuMasterShifu/janusminer/releases/download/0.2.5
 rm -r /home/user/janusminer_hiveos/*
 tar xzvf janusminer_hiveos-0.2.5.tgz
 /hive/sbin/amd-oc
-screen -dmS wart_miner bash -c  "while true; do /home/user/janusminer_hiveos/wart-miner -a walletaddress -h nodeipaddressvar -p nodeipport --threads=8 2>&1 | tee /var/log/janusminer_hiveos.log; done"
+screen -dmS wart_miner bash -c  "while true; do /home/user/janusminer_hiveos/wart-miner -a walletaddress -h nodeipaddressvar -p nodeipport --threads=11 2>&1 | tee /var/log/janusminer_hiveos.log; done"
 
