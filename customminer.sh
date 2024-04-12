5546485fafe4161acdfc330dd7dd587759522896
kernelpanic007 <josalamanca007@gmail.com>
2024-04-12 14:19:48 -0500

    Update customminer.sh

--- customminer.sh
+++ customminer.sh
@@ -4,7 +4,7 @@ screen -S wart_miner -X quit
 worker='"$HOSTNAME"'
 cd /home/user
 wget -nc https://bzminer.com/downloads/bzminer_v21.1.0b4_linux.tar.gz
-rm -r /home/user/bzminer_v21.0.0b4_linux/*
-tar xzvf bzminer_v21.0.0b4_linux.tar.gz
+rm -r /home/user/bzminer_v21.1.0b4_linux/*
+tar xzvf bzminer_v21.1.0b4_linux.tar.gz
 /hive/sbin/amd-oc
-screen -dmS wart_miner bash -c  "while true; do /home/user/bzminer_v21.0.0b4_linux/bzminer -a warthog -r $worker -w walletaddress -p nodeipaddressvar --nc 1 --nvidia 0 --amd 1 --warthog_cpu_threads 12 --warthog_max_ram_gb 4 2>&1 | tee /var/log/bzminer_hiveos.log; done"
+screen -dmS wart_miner bash -c  "while true; do /home/user/bzminer_v21.1.0b4_linux/bzminer -a warthog -r $worker -w walletaddress -p nodeipaddressvar --nc 1 --nvidia 0 --amd 1 --warthog_cpu_threads 12 --warthog_max_ram_gb 4 2>&1 | tee /var/log/bzminer_hiveos.log; done"
