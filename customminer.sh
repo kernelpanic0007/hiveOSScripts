f657802f8c830afb0cd357d9a35cdc2759f4fd28
kernelpanic007 <josalamanca007@gmail.com>
2025-02-19 22:30:01 -0600

    Update customminer.sh

--- customminer.sh
+++ customminer.sh
@@ -3,8 +3,8 @@ screen -S wart_miner -X quit
 #download if necessary and run miner
 worker='"$HOSTNAME"'
 cd /home/user
-wget -nc https://bzminer.com/downloads/bzminer_v23.0.2b3_linux.tar.gz
-rm -r /home/user/bzminer_v23.0.2b3_linux/*
-tar xzvf bzminer_v23.0.2b3_linux.tar.gz
+wget -nc https://bzminer.com/downloads/bzminer_v22.0.0_linux.tar.gz
+rm -r /home/user/bzminer_v22.0.0_linux/*
+tar xzvf bzminer_v22.0.0_linux.tar.gz
 /hive/sbin/amd-oc
-screen -dmS wart_miner bash -c  "while true; do /home/user/bzminer_v23.0.2b3_linux/bzminer -a warthog -r $worker -w walletaddress -p nodeipaddressvar --nc 1 --nvidia 0 --amd 1 --cpu_threads 12 --cpu_threads_start_offset 0 --warthog_max_ram_gb 0 --warthog_verus_hr_target 0 2>&1 | tee /var/log/bzminer_hiveos.log; done"
+screen -dmS wart_miner bash -c  "while true; do /home/user/bzminer_v22.0.0_linux/bzminer -a warthog -r $worker -w walletaddress -p nodeipaddressvar --nc 1 --nvidia 0 --amd 1 --cpu_threads 12 --cpu_threads_start_offset 0 --warthog_max_ram_gb 0 --warthog_verus_hr_target 0 2>&1 | tee /var/log/bzminer_hiveos.log; done"
